98cb978caa4f2960ccf959ecec1b2829fffd477a
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-06 22:08:19 +0300

    string.?: add string reversal and int to string conversion

--- src/string.c
+++ src/string.c
@@ -8,3 +8,47 @@ size_t string_len(const char* str)  {
 
 	return len;
 }
+
+void string_rev(char str[], size_t len) {
+    size_t begin = 0, end = len - 1;
+
+    while (begin < end) {
+        const char temp = str[begin];
+
+        str[begin++] = str[end];
+        str[end--] = temp;
+    }
+}
+
+void string_itoa(int a, char str[], int base) {
+    int i = 0;
+
+    if (a == 0) {
+        str[i++] = 0;
+        str[i] = '\0';
+    }
+
+    bool negative = 0;
+    if (a < 0 && base == 10) {
+        negative = 1;
+        a = -a;
+    }
+
+    while (a != 0) {
+        int r = a % base;
+        str[i++] = (r < 10) ? r + '0' : (r - 10) + 'a'; 
+
+        a /= base;
+    }
+
+    if (negative)
+        str[i++] = '-';
+    
+    str[i] = '\0';
+
+    string_rev(str, i);
+}
+
+void string_itoa_10(int a, char str[]) {
+    string_itoa(a, str, 10);
+}
--- src/string.h
+++ src/string.h
@@ -3,7 +3,15 @@
 #define STRING_H
 
 #include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 size_t string_len(const char* str);
+
+void string_rev(char str[], size_t len);
+
+void string_itoa(int a, char str[], int base);
+
+void string_itoa_10(int a, char str[]);
 
 #endif
