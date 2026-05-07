a49a8d4ccb6a732a1fb7fd9cd071c026fd6e6f80
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-07 20:06:47 +0300

    kernel.c: center welcome message

--- src/kernel.c
+++ src/kernel.c
@@ -7,6 +7,8 @@ extern const char* SABAL_PALMETTO_ASCII;
 void kernel_main(void) {
     console_init();
 
-    console_write("Welcome to Sabal OS!\n\n");
+    console_write_centered("Welcome to Sabal OS!");
+    
+    console_write("\n\n");
     console_write(SABAL_PALMETTO_ASCII);
 }
