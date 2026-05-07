484379103285b70df72acb0ae4dff3617e0ea805
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-07 20:09:13 +0300

    kernel.c: output newline before welcome message

--- src/kernel.c
+++ src/kernel.c
@@ -7,6 +7,7 @@ extern const char* SABAL_PALMETTO_ASCII;
 void kernel_main(void) {
     console_init();
 
+    console_write("\n");
     console_write_centered("Welcome to Sabal OS!");
     
     console_write("\n\n");
