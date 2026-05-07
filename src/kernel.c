a3a72107645f7316711a47198363b6232c7bbf9d
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-07 19:54:47 +0300

    kernel.c: output a welcome message and ASCII art of a sabal tree

--- src/kernel.c
+++ src/kernel.c
@@ -2,8 +2,11 @@
 
 #include "console.h"
 
+extern const char* SABAL_PALMETTO_ASCII;
+
 void kernel_main(void) {
     console_init();
 
-    console_write("Hello, world!");
+    console_write("Welcome to Sabal OS!\n\n");
+    console_write(SABAL_PALMETTO_ASCII);
 }
