4b39987393dd2171984efefb30899c234c05dc8c
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-12 20:28:53 +0300

    kernel.c: cleanup

--- src/kernel.c
+++ src/kernel.c
@@ -2,13 +2,13 @@
 #include "console.h"
 
 extern const char* SABAL_PALMETTO_ASCII;
+extern const char* SABAL_HEADER_ASCII;
 
 void kernel_main(void) {
     console_init();
 
     console_write("\n");
-    console_write_centered("Welcome to Sabal OS!");
+    console_write_centered("Welcome to Sabal OS!\n\n");
     
-    console_write("\n\n");
     console_write(SABAL_PALMETTO_ASCII);
 }
