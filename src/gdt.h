3ac78520ac72736e542d3128e14c6d1f8cfd1a23
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-16 12:37:32 +0300

    gdt.h: add segment descriptor struct

--- src/gdt.h
+++ src/gdt.h
@@ -2,6 +2,19 @@
 #ifndef GDT_H
 #define GDT_H
 
+#include <stdint.h>
 
+typedef struct {
+    /* Lower dword */
+    uint16_t limit_l;
+    uint16_t base_l;
+
+    /* Upper dword */
+    uint8_t base_m;
+    uint8_t access;
+    uint8_t limit_u_flags;
+    uint8_t base_u;
+
+} gdt_seg_t __attribute__ ((packed));
 
 #endif
