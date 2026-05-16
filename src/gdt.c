c7cd4e6bc8be65199311bf937d11e1de37cdd1cf
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-16 12:41:13 +0300

    gdt.c: implement function for packing segment descriptors

--- src/gdt.c
+++ src/gdt.c
@@ -1,2 +1,20 @@
 
 #include "gdt.h"
+
+gdt_seg_t gdt_entry(uint32_t limit, uint32_t base, uint8_t access, uint8_t flags) {
+    gdt_seg_t entry;
+
+    /* Lower dword */
+    entry.limit_l = (uint16_t) limit;
+    entry.base_l = (uint16_t) base;
+
+    /* Upper dword */
+    entry.base_m = (uint8_t) (base >> 16);
+    
+    entry.access = access;
+
+    entry.limit_u_flags = (flags << 4) | ((limit >> 16) & 0xF);
+    entry.base_u = (uint8_t) (base >> 24);
+
+    return entry;
+}
