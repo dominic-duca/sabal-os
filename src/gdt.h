44a150f8e4be1363e179f15d7d542b5b000377d7
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-16 14:08:39 +0300

    gdt.h: add function prototype for initializing GDT

--- src/gdt.h
+++ src/gdt.h
@@ -26,6 +26,8 @@ typedef struct {
 
 } __attribute__ ((packed)) gdt_seg_t;
 
+void gdt_init(void);
+
 void gdt_update_reg(void);
 
 gdt_seg_t gdt_entry(uint32_t limit, uint32_t base, uint8_t access, uint8_t flags);
