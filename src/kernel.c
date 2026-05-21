2fcbfaf92f11cb532c381c62797097a6c3d5dc15
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-21 22:34:54 +0300

    kernel.c: initialize the IDT

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,14 @@
 
 #include "gdt.h"
+#include "idt.h"
+
 #include "console.h"
 
 extern const char* SABAL_PALMETTO_ASCII;
 
 void kernel_main(void) {
     gdt_init();
+    idt_init();
     
     console_init();
     console_write_centered("\nWelcome to Sabal OS!\n");
