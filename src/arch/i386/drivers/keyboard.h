f952c96ad2b898821a92627dd4ab15dc30582ef4
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-01 00:55:54 +0300

    keyboard.h: add lookup array for PS/2 Scancode Set 1 to ASCII

--- src/arch/i386/drivers/keyboard.h
+++ src/arch/i386/drivers/keyboard.h
@@ -8,4 +8,13 @@
 
 #define KEYBOARD_PS2_IRQ    0x01
 
+static const char keyboard_scancode_ascii[] = {
+    0, '\e', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
+    0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0,
+    '*', 0, ' ', 0,
+    /* ... function keys and more here */
+};
+
 #endif
