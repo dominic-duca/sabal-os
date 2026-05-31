70b54f8511e77c653086e3bb728d97e5a7cca33e
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-31 23:07:00 +0300

    pic.h: define 8086 mode, cascade IRQ and init contants

--- src/arch/i386/irq/pic.h
+++ src/arch/i386/irq/pic.h
@@ -4,10 +4,16 @@
 
 #include "../../../io.h"
 
-#define PIC1_COMMAND    0x20
-#define PIC1_DATA       0x21       
+#define PIC_1_COMMAND   0x20
+#define PIC_1_DATA      0x21       
 
-#define PIC2_COMMAND    0xA0
-#define PIC2_DATA       0xA1
+#define PIC_2_COMMAND   0xA0
+#define PIC_2_DATA      0xA1
+
+#define PIC_MODE_8086   0x01
+
+#define PIC_IRQ_CASCADE 0x02
+
+#define PIC_INIT        0x11
 
 #endif
