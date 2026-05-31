0f244e734ca162498d9202913a04352f87c7ff76
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-05-31 22:08:37 +0300

    pic.h: define 8259 PIC port values

--- src/arch/i386/irq/pic.h
+++ src/arch/i386/irq/pic.h
@@ -2,6 +2,12 @@
 #ifndef PIC_H
 #define PIC_H
 
+#include "../../../io.h"
 
+#define PIC1_COMMAND    0x20
+#define PIC1_DATA       0x21       
+
+#define PIC2_COMMAND    0xA0
+#define PIC2_DATA       0xA1
 
 #endif
