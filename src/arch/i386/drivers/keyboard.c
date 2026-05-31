19f044f982fc8927139cf77953dbf4fb0775eb8e
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-01 01:44:32 +0300

    keyboard.c: pass the pressed key to keyboard_callback in ASCII form

--- src/arch/i386/drivers/keyboard.c
+++ src/arch/i386/drivers/keyboard.c
@@ -11,7 +11,7 @@ void keyboard_handler(void) {
     uint8_t scancode = io_inb(KEYBOARD_PS2_DATA);
     
     if (scancode < sizeof(keyboard_scancode_ascii)) {
-        /* Do something with keyboard_scancode_ascii[scancode] */
+        keyboard_callback(keyboard_scancode_ascii[scancode]);
     }
 
     pic_irq_eoi(KEYBOARD_PS2_IRQ);
