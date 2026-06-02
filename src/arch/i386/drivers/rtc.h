8d4f3d182378af4d4432a3116b72babd35603b9e
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-02 17:17:17 +0300

    rtc.h: add prototype that returns current datetime

--- src/arch/i386/drivers/rtc.h
+++ src/arch/i386/drivers/rtc.h
@@ -4,9 +4,13 @@
 
 #include "../irq/pic.h"
 
+#include "../../../util/datetime.h"
+
 #define RTC_IRQ 8
 
 typedef void (*rtc_callback_t) (void);
+
+datetime_t rtc_get_datetime(void);
 
 void rtc_set_callback(rtc_callback_t callback);
 
