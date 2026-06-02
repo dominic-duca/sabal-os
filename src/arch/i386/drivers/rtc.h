f97cbee006c25c3328a78754dd4bc8e838f6f988
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-02 17:08:18 +0300

    rtc.h: define IRQ line, add handler and callback setter

--- src/arch/i386/drivers/rtc.h
+++ src/arch/i386/drivers/rtc.h
@@ -2,6 +2,14 @@
 #ifndef RTC_H
 #define RTC_H
 
+#include "../irq/pic.h"
 
+#define RTC_IRQ 8
+
+typedef void (*rtc_callback_t) (void);
+
+void rtc_set_callback(rtc_callback_t callback);
+
+void rtc_handler(void);
 
 #endif
