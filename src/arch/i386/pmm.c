42d883f64e57c6cd824d7171e82df51833327f06
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-09 23:23:47 +0300

    pmm: add bitmap for tracking allocated pages

--- src/arch/i386/pmm.c
+++ src/arch/i386/pmm.c
@@ -1,2 +1,10 @@
 
 #include "pmm.h"
+
+static uint32_t pmm_bitmap[PMM_BITMAP_LIMIT];
+
+void pmm_init(void) {
+    for (int i = 0; i < PMM_BITMAP_LIMIT; i++) {
+        pmm_bitmap[i] = 0xFFFFFFFF;
+    }
+}
--- src/arch/i386/pmm.h
+++ src/arch/i386/pmm.h
@@ -2,6 +2,9 @@
 #ifndef PMM_H
 #define PMM_H
 
+#include <stdint.h>
 
+/* (32768 * 32) pages * 4 KiB/page = 4 GiB */
+#define PMM_BITMAP_LIMIT 32768
 
 #endif
