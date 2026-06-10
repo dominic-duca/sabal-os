1bc3b14d4196da6a13d6cf72f054bcba68d66984
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-10 17:46:08 +0300

    bit.h: add helper for aligning down to powers of 2

--- src/util/bit.h
+++ src/util/bit.h
@@ -6,4 +6,8 @@ static inline unsigned int bit_align_up(unsigned int a, unsigned int b) {
     return (a + (b - 1)) & ~(b - 1);
 }
 
+static inline unsigned int bit_align_down(unsigned int a, unsigned int b) {
+    return a & ~(b - 1);
+}
+
 #endif
