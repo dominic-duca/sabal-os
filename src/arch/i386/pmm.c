eecd34d1ae549a60cd13e35a39bacf970ba3678d
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-10 17:59:00 +0300

    pmm: add function to free chunks of pages

--- src/arch/i386/pmm.c
+++ src/arch/i386/pmm.c
@@ -12,3 +12,20 @@ void pmm_init(void) {
 void pmm_free_page(uint32_t page) {
     pmm_bitmap[page / 32] &= ~((uint32_t) 0x01 << (page % 32));
 }
+
+void pmm_free_pages(uint32_t begin, uint32_t end) {
+    /* Free individual pages */
+    for (uint32_t page = begin; page < bit_ceil(begin, 32); page++) {
+        pmm_free_page(page);
+    }
+
+    /* Free in chunks of 32 */
+    for (uint32_t i_bitmap = bit_ceil(begin, 32) / 32; i_bitmap < bit_floor(end, 32) / 32; i_bitmap++) {
+        pmm_bitmap[i_bitmap] = 0x00;
+    }
+
+    /* Free individual pages */
+    for (uint32_t page = bit_floor(end, 32); page <= end; page++) {
+        pmm_free_page(page);
+    }
+}
--- src/arch/i386/pmm.h
+++ src/arch/i386/pmm.h
@@ -4,10 +4,14 @@
 
 #include <stdint.h>
 
+#include "../../util/bit.h"
+
 /* (32768 * 32) pages * 4 KiB/page = 4 GiB */
 #define PMM_BITMAP_LIMIT 32768
 
 void pmm_init(void);
+
 void pmm_free_page(uint32_t page);
+void pmm_free_pages(uint32_t begin, uint32_t end);
 
 #endif
