0d6fd8538ff3a75837901a7a0c70693416d04d8f
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-21 16:41:56 +0300

    pmm: add alloc counterparts of free_page/free_pages

--- src/arch/i386/pmm.c
+++ src/arch/i386/pmm.c
@@ -17,6 +17,10 @@ void pmm_free_page(uint32_t page) {
     pmm_bitmap[page / 32] &= ~((uint32_t) 0x01 << (page % 32));
 }
 
+void pmm_alloc_page(uint32_t page) {
+    pmm_bitmap[page / 32] |= (uint32_t) 0x01 << (page % 32);
+}
+
 void pmm_free_pages(uint32_t begin, uint32_t len) {
     const uint32_t end = begin + len;
         
@@ -36,6 +40,27 @@ void pmm_free_pages(uint32_t begin, uint32_t len) {
     }
 
     pmm_free_count += len;
+}
+
+void pmm_alloc_pages(uint32_t begin, uint32_t len) {
+    const uint32_t end = begin + len;
+        
+    /* Allocate individual pages */
+    for (uint32_t page = begin; page < bit_ceil(begin, 32); page++) {
+        pmm_alloc_page(page);
+    }
+
+    /* Allocate in chunks of 32 */
+    for (uint32_t i_bitmap = bit_ceil(begin, 32) / 32; i_bitmap < bit_floor(end, 32) / 32; i_bitmap++) {
+        pmm_bitmap[i_bitmap] = 0xFFFFFFFF;
+    }
+
+    /* Allocate individual pages */
+    for (uint32_t page = bit_floor(end, 32); page < end; page++) {
+        pmm_alloc_page(page);
+    }
+
+    pmm_free_count -= len;
 }
 
 uint16_t pmm_get_free_count(void) {
