8d38b6161d7561bbe6f5477f1f2f2a07505fe088
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-23 20:05:41 +0300

    vmm: declare page dir and count of entries

--- src/arch/i386/vmm.c
+++ src/arch/i386/vmm.c
@@ -1,2 +1,5 @@
 
 #include "vmm.h"
+
+static uint32_t vmm_dir[1024] __attribute__((aligned(4096)));
+static size_t   vmm_dir_size;
--- src/arch/i386/vmm.h
+++ src/arch/i386/vmm.h
@@ -2,4 +2,7 @@
 #ifndef VMM_H
 #define VMM_H
 
+#include <stddef.h>
+#include <stdint.h>
+
 #endif
