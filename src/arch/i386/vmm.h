1ceaa392d0b1375d56e25e2f8ee098555d4a0f22
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-23 21:56:25 +0300

    vmm.h: add helper for packing dir and table entries

--- src/arch/i386/vmm.h
+++ src/arch/i386/vmm.h
@@ -5,6 +5,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define VMM_ENTRY_PRESENT 0x01
+#define VMM_ENTRY_WRITE   0x02
+#define VMM_ENTRY_USER    0x04
+
+static inline uint32_t vmm_entry(uint32_t addr, uint16_t flags) {
+    return (addr & 0xFFFFF000) | (flags & 0x0FFF);
+}
+
 void vmm_init(void);
 
 #endif
