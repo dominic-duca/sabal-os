e6659cf7666f7d4bea5d320d2a1b470390db4e94
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-23 20:07:19 +0300

    vmm: add init function

--- src/arch/i386/vmm.c
+++ src/arch/i386/vmm.c
@@ -3,3 +3,11 @@
 
 static uint32_t vmm_dir[1024] __attribute__((aligned(4096)));
 static size_t   vmm_dir_size;
+
+void vmm_init(void) {
+    for (size_t i = 0; i < 1024; i++) {
+        vmm_dir[i] = 0x00000000;
+    }
+    
+    vmm_dir_size = 0;
+}
--- src/arch/i386/vmm.h
+++ src/arch/i386/vmm.h
@@ -5,4 +5,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
+void vmm_init(void);
+
 #endif
