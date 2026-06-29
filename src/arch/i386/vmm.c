50ba825a149b487fd173656ad6638e6dc9fe5c49
Dominic Duca <251473418+domidq@users.noreply.github.com>
2026-06-29 23:48:15 +0300

    vmm.c: add initial page table

--- src/arch/i386/vmm.c
+++ src/arch/i386/vmm.c
@@ -2,6 +2,7 @@
 #include "vmm.h"
 
 static uint32_t vmm_dir[1024] __attribute__((aligned(4096)));
+static uint32_t vmm_table[1024] __attribute__((aligned(4096)));
 
 void vmm_dir_insert(uint16_t index, uint32_t entry) {
     vmm_dir[index] = entry;
